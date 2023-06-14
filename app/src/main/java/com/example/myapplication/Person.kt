b650b0a1338fb85ee2a4f8b9caf00ebfd637d3b4
zbw <1552798761@qq.com>
2023-06-14 22:25:28 +0800

    kotlin类继承

--- app/src/main/java/com/example/myapplication/Person.kt
+++ app/src/main/java/com/example/myapplication/Person.kt
@@ -2,19 +2,32 @@ package com.example.myapplication
 
 
 // open使得类可以被继承
-open class Person{
-    var name = ""
-    var age = 0
-
+//()里是主构造函数，无函数体
+open class Person(val name : String, val age : Int){
     fun eat(){
         println(name + " is eating. he is "+ age +" year old.")
     }
 }
 
+class Student(val sex:String,name: String,age: Int):Person(name,age){
+    // 主构造函数体
+    init{
+        println(name + " gender is" + sex)
+    }
+
+    // 次构造函数
+    constructor():this("??","无名氏",0)
+    constructor(name: String,age: Int):this("??",name,age)
+
+}
 
 fun main(){
-    var p =  Person()
-    p.name = "zbw"
-    p.age = 21
+    var p =  Person("zbw",21)
     p.eat()
+    var s = Student("女","lcx",21)
+    s.eat()
+    var s2 = Student()
+    s2.eat()
+    var s3 = Student("赫敏",16)
+    s3.eat()
 }
